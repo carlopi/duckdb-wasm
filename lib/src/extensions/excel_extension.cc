3b2716bc91817f1b01df0727f5c308058fe4fa3f
Carlo Piovesan <piovesan.carlo@gmail.com>
2023-03-17 09:46:50 +0100

    remove explicit extensions code

--- lib/src/extensions/excel_extension.cc
+++
@@ -1,5 +0,0 @@
-#include "duckdb/web/extensions/excel_extension.h"
-
-#include "excel-extension.hpp"
-
-extern "C" void duckdb_web_excel_init(duckdb::DuckDB* db) { db->LoadExtension<duckdb::EXCELExtension>(); }
