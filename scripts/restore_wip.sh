352844dd84201751516888046456b5cc2b2b78ec
Carlo Piovesan <piovesan.carlo@gmail.com>
2023-04-25 12:34:11 +0200

    Add also duckdblabs

--- scripts/restore_wip.sh
+++ scripts/restore_wip.sh
@@ -2,3 +2,9 @@ cd submodules/duckdb
 git apply ../../duckdb.patch
 cd extension
 git clone https://github.com/carlopi/quackMcWasm
+mkdir duckdblabs
+head -n 14 CMakeLists.txt > duckdblabs/CMakeLists.txt
+cd duckdblabs
+git clone https://github.com/duckdblabs/sqlite_scanner
+git clone https://github.com/duckdblabs/postgres_scanner
+git clone https://github.com/duckdblabs/substrait
