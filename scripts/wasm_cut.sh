e6357a808c13920ee01002d1d429f52e9be6e9e8
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-09-04 14:33:25 +0200

    X

--- scripts/wasm_cut.sh
+++ scripts/wasm_cut.sh
@@ -1,7 +1,7 @@
 ls -la *
-wasm2wat --enable-all duckdb_wasm-$1.wasm -o duckdb_wat.wat
+wasm2wat --enable-all duckdb-$1.wasm -o duckdb_wat.wat
 ls -la duckdb_wasm-$1.wasm
 sed -i "/export*Executor/c\" ${BUILD_DIR}/duckdb_wat.wat
-wat2wasm --enable-all duckdb_wat.wat -o duckdb_wasm-$1.wasm
+wat2wasm --enable-all duckdb_wat.wat -o duckdb-$1.wasm
 rm duckdb_wat.wat
 ls -la duckdb_wasm-$1.wasm
