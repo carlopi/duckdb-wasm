886d978bda904f4a1185d7b4ef548bc1b3516696
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-09-04 15:26:54 +0200

    X

--- scripts/wasm_cut.sh
+++ scripts/wasm_cut.sh
@@ -1,9 +1,11 @@
 ls -la *
 wasm2wat --enable-all duckdb-$1.wasm -o duckdb_wat.wat
 ls -la duckdb-$1.wasm
-sed -i "/export*Executor/c\" duckdb_wat.wat
 ls -la duckdb_wat.wat
-wat2wasm --enable-all duckdb_wat.wat -o duckdb-$1.wasm
-ls -la duckdb_wat.wat
+#sed -i "/export*Executor/c\" duckdb_wat.wat
+grep -v -E 'export.*Executor' duckdb_wat.wat > duckdb_wat_filtered.wat
+ls -la duckdb_wat_filtered.wat
+wat2wasm --enable-all duckdb_wat_filtered.wat -o duckdb-$1.wasm
+ls -la duckdb_wat_filtered.wat
 rm duckdb_wat.wat
 ls -la duckdb-$1.wasm
