77625bc36e69750d636a62fc16b9ee285fd56f5c
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-09-04 10:53:23 +0200

    Move to separate script

---
+++ scripts/wasm_cut.sh
@@ -0,0 +1,6 @@
+wasm2wat --enable-all duckdb_wasm.wasm -o duckdb_wat.wat
+ls -la duckdb_wasm.wasm
+sed -i "/export*Executor/c\" ${BUILD_DIR}/duckdb_wat.wat
+wat2wasm --enable-all duckdb_wat.wat -o duckdb_wasm.wasm
+rm duckdb_wat.wat
+ls -la duckdb_wasm.wasm
